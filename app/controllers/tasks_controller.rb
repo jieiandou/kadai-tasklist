7d58bfc431fab801e302e16648415298cf31c248
jiei.andou <ando@blade.mp.es.osaka-u.ac.jp>
2022-11-12 08:40:49 +0000

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,6 @@ class TasksController < ApplicationController
   end
 
   def show
-   set_task
   end
 
   def new
@@ -25,11 +24,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
   end
 
   def update
-    @task = Task.find(params[:id])
+    
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_task
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
